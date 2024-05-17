d5b29698d5a4fa2933fbdb65e94c15caa8b8a34e
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-05-17 11:34:49 +0300

    Added information about each single course and the same link back to return to the page of all courses

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,7 +1,25 @@
+import courses from '../data/course';
+import { Link, useParams } from 'react-router-dom';
 const SingleCourse = () => {
+  // Хук useParams используется для получения текущих параметров маршрута
+  const params = useParams();
+  // Ищем объект slug которого совпадает с значением объекта params(текущем параметром)
+  const course = courses.find((course) => course.slug === params.courseSlug);
+  // Логируем параметры маршрута в консоль
+  console.log(params);
   return (
     <>
+      {/* Выводим данные о курсе */}
       <h1>Single course</h1>
+      <h2>{course.title}</h2>
+      <h3>{course.slug}</h3>
+      <h3>{course.id}</h3>
+
+      {/* Ссылка на Courses */}
+      {/* .. - на уровень выше, relative="path" - гарантирует, что ссылка будет работать относительно текущего пути */}
+      <Link to=".." relative="path">
+        All courses
+      </Link>
     </>
   );
 };
