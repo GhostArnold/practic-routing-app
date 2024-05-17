d6239699b027b2a70da12db08f8204bc180cd7bd
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-05-17 09:49:42 +0300

    I made subcategories for the nav bar element with a transition to slug routes linked to them

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,7 +1,20 @@
+// Делаем импорт массива объектов
+import { Link } from 'react-router-dom';
+import courses from '../data/course';
+
 const Courses = () => {
   return (
     <>
       <h1>Courses</h1>
+      {/* Используем метод map для итерации по массиву courses. Course - текущий объект */}
+      {courses.map((course) => {
+        return (
+          // to - маршрут, className - для стилизации, course.title - выводим названия наших курсов
+          <Link to={course.slug} key={course.id} className="courseLink">
+            {course.title}
+          </Link>
+        );
+      })}
     </>
   );
 };
